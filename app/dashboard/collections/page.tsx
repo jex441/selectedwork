2a5b38a1c64ed16f2e4fbe2235e0b5f8e6c3d2ed
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-06-18 20:47:08 -0400

    page settings;

--- app/dashboard/collections/page.tsx
+++ app/dashboard/collections/page.tsx
@@ -7,6 +7,7 @@ export default function Invoices() {
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">2023</h1>
+        <span>Page Settings</span>
         <Button className="ml-auto" size="sm">
           Upload Image
         </Button>
