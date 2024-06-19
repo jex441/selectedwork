d3453f8da847729da38e2e0e19e420609de200a4
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-06-19 17:41:06 -0400

    login

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ import { Button } from '@/components/ui/button';
 
 export default function Page() {
   return (
-    <div className="flex min-h-[100dvh] flex-col">
-      <main className="flex-1 bg-gray-100 dark:bg-gray-950">
-        <div className="container mx-auto grid max-w-6xl grid-cols-1 gap-12 px-4 py-12 md:grid-cols-2 md:gap-24 md:px-6 lg:py-24">
+    <div className="mx-auto flex min-h-[100dvh] w-full flex-col">
+      <main className="flex bg-gray-100 dark:bg-gray-950">
+        <div className="mx-auto grid max-w-6xl grid-cols-1 gap-12 px-4 py-12  md:gap-24 md:px-6 lg:py-24">
           <div className="grid gap-6">
             <Card>
               <CardHeader>
@@ -89,6 +89,7 @@ export default function Page() {
           </div>
         </div>
       </main>
+
       <footer className="bg-gray-100 py-6 dark:bg-gray-800">
         <div className="container mx-auto flex max-w-6xl items-center justify-between px-4 md:px-6">
           <p className="text-sm text-gray-500 dark:text-gray-400">
