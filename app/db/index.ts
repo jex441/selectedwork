193d96b449099d28d7b58acc9d9f11df472e9a9b
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-06-24 20:24:47 -0400

    db

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,7 +1,8 @@
-import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { config } from 'dotenv';
+import { neon } from '@neondatabase/serverless';
 
 config({ path: '.env.local' });
 
+const sql = neon(process.env.POSTGRES_URL!)
 export const db = drizzle(sql);
