31f6c8e7daeb1b45989152fdb383ebd7195f2416
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-07-29 14:18:54 -0400

    less margin

--- app/[username]/[page]/work/page.tsx
+++ app/[username]/[page]/work/page.tsx
@@ -7,7 +7,7 @@ export default function Work({ data }: {}) {
   console.log(data);
   return (
     <main className="flex w-full flex-wrap justify-center">
-      <section className="my-10 flex w-full flex-col justify-center lg:mx-20 lg:flex-row">
+      <section className="my-5 flex w-full flex-col justify-center lg:mx-20 lg:flex-row">
         {data.imgSrc && (
           <>
             <div className="mx-1 flex h-[350px] flex-col lg:m-5 lg:mx-0 lg:h-[400px] lg:w-1/2 lg:w-[500px]">
@@ -30,7 +30,7 @@ export default function Work({ data }: {}) {
         )}
 
         <div className="mx-1 my-5 flex-1 lg:m-5 lg:w-1/2">
-          <h1 className="text-xl leading-9">{data.title}</h1>
+          <h1 className="text-2xl leading-9">{data.title}</h1>
           <h3 className="text-sm leading-7 text-gray-600">{data.subheading}</h3>
           <p className="text-sm leading-7 text-gray-600">{data.description}</p>
           <p className="my-4 text-sm text-gray-600">
