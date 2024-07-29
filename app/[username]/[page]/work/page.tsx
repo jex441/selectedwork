fb02a82bf770747d15ff26fe4ebb2386cae67a38
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-07-29 14:02:06 -0400

    collection heading

--- app/[username]/[page]/work/page.tsx
+++ app/[username]/[page]/work/page.tsx
@@ -1,12 +1,40 @@
 'use client';
+import Image from 'next/image';
 
 import React from 'react';
 import Piece from './piece';
-
 export default function Work({ data }: {}) {
+  console.log(data);
   return (
     <main className="flex w-full flex-wrap justify-center">
-      {data && data.works.map((work) => <Piece data={work} />)}
+      <section className="my-10 flex w-full flex-col justify-center lg:mx-20 lg:flex-row">
+        <div className="mx-1 flex h-[350px] flex-col lg:m-5 lg:mx-0 lg:h-[400px] lg:w-1/2 lg:w-[500px]">
+          <div className="relative h-full w-full">
+            <Image
+              fill
+              src={data.imgSrc}
+              objectFit="contain"
+              style={{ objectFit: 'contain' }}
+              alt="cover"
+            />
+          </div>
+          <div className="mt-2">
+            <span className="text-sm italic text-gray-600">
+              {data.imgCaption}
+            </span>
+          </div>
+        </div>
+        <div className="mx-1 my-5 w-full lg:m-5 lg:w-1/2">
+          <h1 className="text-xl leading-9">{data.title}</h1>
+          <h3 className="text-sm leading-7 text-gray-600">{data.subheading}</h3>
+          <p className="text-sm leading-7 text-gray-600">{data.description}</p>
+          <p className="text-sm leading-7 text-gray-600">{data.linkText1}</p>
+          <p className="text-sm leading-7 text-gray-600">{data.linkText2}</p>
+        </div>
+      </section>
+      <section className="flex w-full flex-wrap justify-center">
+        {data && data.works.map((work) => <Piece data={work} />)}
+      </section>
     </main>
   );
 }
--- app/[username]/[page]/work/piece.tsx
+++ app/[username]/[page]/work/piece.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function piece({ data }: {}) {
   return (
-    <section className="border-1 relative m-2 grid h-auto w-full justify-items-stretch lg:m-5 lg:h-[290px] lg:w-[305px]">
+    <section className="border-1 relative mx-1 mb-6 grid h-auto w-full justify-items-stretch lg:m-3 lg:h-[290px] lg:w-[305px]">
       <figure className="relative h-[400px] w-full self-center justify-self-center object-contain lg:h-[230px] lg:w-[305px]">
         <Image
           fill
@@ -13,7 +13,7 @@ export default function piece({ data }: {}) {
           src={data.media[0].url}
         />
       </figure>
-      <div className="flex w-[305px] self-end justify-self-center text-sm tracking-wide ">
+      <div className="mt-4 flex w-full self-end justify-self-center text-xs tracking-wide lg:mt-0 lg:w-[305px] ">
         <span className="uppercase italic text-gray-600">{data.title}</span>
         <span className="ml-3 text-gray-400">
           {data.year && `${data.year}`}
