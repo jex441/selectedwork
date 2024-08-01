00842eb9fba4ec935630197b632d074b3134515c
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-01 15:27:32 -0400

    justify-start

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -42,12 +42,14 @@ export default async function Work({
       <section className="my-5 flex w-full flex-col justify-center lg:mx-20 lg:flex-row">
         {imgSrc && (
           <>
-            <div className="mx-1 flex h-[350px] flex-col lg:m-5 lg:mx-0 lg:h-[400px] lg:w-1/2 lg:w-[500px]">
-              <div className="relative h-full w-full">
+            <div className="mx-1 flex flex-col lg:m-5 lg:mx-0 lg:h-[400px] lg:w-1/2 lg:w-[500px]">
+              <div className="relative w-full">
                 <Image
-                  fill
+                  height={0}
+                  width={0}
                   src={imgSrc}
-                  style={{ objectFit: 'contain' }}
+                  sizes="100vw"
+                  className="w-full object-contain"
                   alt="cover"
                 />
               </div>
@@ -89,7 +91,7 @@ export default async function Work({
         </div>
       </section>
       <section className="flex w-full flex-wrap justify-between gap-y-10 lg:p-10">
-        {works && works.map((work) => <Piece data={work} />)}
+        {works && works.map((work) => <Piece key={work.id} data={work} />)}
       </section>
     </main>
   );
