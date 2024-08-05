03751d91c80734906b03229ecd2ce13a8850f70d
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-05 07:51:20 -0400

    cv page is component

---
+++ app/[username]/cv/CVPage.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { getCVPageDataForSite } from '../../lib/data';
+import { ICVPage } from '../../interfaces/ICVPage';
+import CVSection from './CVSection';
+
+export default function CVPage({ data }: { data: ICVPage }) {
+  return (
+    <main className="flex w-full flex-col items-center justify-center lg:flex-row">
+      {/* <section className="relative m-1 flex h-[300px] w-full flex-col object-contain lg:m-10 lg:h-[490px] lg:w-1/2">
+    <div className="relative h-[320px] w-full lg:h-[480px] "></div>
+    <div className="w-full text-sm text-gray-700">{imgCaption}</div>
+  </section> */}
+
+      <section className="flex w-full flex-col items-center justify-center lg:mt-10 lg:w-3/4">
+        {data !== null && data.education.length > 0 && (
+          <CVSection data={data} heading="Education" categoryId="education" />
+        )}
+        {data !== null && data.soloExhibitions.length > 0 && (
+          <CVSection
+            data={data}
+            heading="Solo Exhibitions"
+            categoryId="soloExhibitions"
+          />
+        )}
+        {data !== null && data.groupExhibitions.length > 0 && (
+          <CVSection
+            data={data}
+            heading="Group Exhibitions"
+            categoryId="groupExhibitions"
+          />
+        )}
+        {data !== null && data.awards.length > 0 && (
+          <CVSection data={data} heading="Awards" categoryId="awards" />
+        )}
+        {data !== null && data.press.length > 0 && (
+          <CVSection data={data} heading="Press" categoryId="press" />
+        )}
+        {data !== null && data.residencies.length > 0 && (
+          <CVSection
+            data={data}
+            heading="Residencies"
+            categoryId="residencies"
+          />
+        )}
+        {data !== null && data.teaching.length > 0 && (
+          <CVSection data={data} heading="Teaching" categoryId="teaching" />
+        )}
+      </section>
+    </main>
+  );
+}
--- app/[username]/cv/page.tsx
+++ app/[username]/cv/page.tsx
@@ -3,6 +3,7 @@ import { Suspense } from 'react';
 import { getCVPageDataForSite } from '../../lib/data';
 import { ICVPage } from '../../interfaces/ICVPage';
 import CVSection from './CVSection';
+import CVPage from './CVPage';
 
 export default async function page({
   params,
@@ -39,58 +40,12 @@ export default async function page({
     'teaching',
   ];
 
+  if (res.data === null) {
+    return 'loading';
+  }
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <main className="flex w-full flex-col items-center justify-center lg:flex-row">
-        {/* <section className="relative m-1 flex h-[300px] w-full flex-col object-contain lg:m-10 lg:h-[490px] lg:w-1/2">
-        <div className="relative h-[320px] w-full lg:h-[480px] "></div>
-        <div className="w-full text-sm text-gray-700">{imgCaption}</div>
-      </section> */}
-
-        <section className="flex w-full flex-col items-center justify-center lg:mt-10 lg:w-3/4">
-          {res.data !== null && res.data.education.length > 0 && (
-            <CVSection
-              data={res.data}
-              heading="Education"
-              categoryId="education"
-            />
-          )}
-          {res.data !== null && res.data.soloExhibitions.length > 0 && (
-            <CVSection
-              data={res.data}
-              heading="Solo Exhibitions"
-              categoryId="soloExhibitions"
-            />
-          )}
-          {res.data !== null && res.data.groupExhibitions.length > 0 && (
-            <CVSection
-              data={res.data}
-              heading="Group Exhibitions"
-              categoryId="groupExhibitions"
-            />
-          )}
-          {res.data !== null && res.data.awards.length > 0 && (
-            <CVSection data={res.data} heading="Awards" categoryId="awards" />
-          )}
-          {res.data !== null && res.data.press.length > 0 && (
-            <CVSection data={res.data} heading="Press" categoryId="press" />
-          )}
-          {res.data !== null && res.data.residencies.length > 0 && (
-            <CVSection
-              data={res.data}
-              heading="Residencies"
-              categoryId="residencies"
-            />
-          )}
-          {res.data !== null && res.data.teaching.length > 0 && (
-            <CVSection
-              data={res.data}
-              heading="Teaching"
-              categoryId="teaching"
-            />
-          )}
-        </section>
-      </main>
+      <CVPage data={res.data} />
     </Suspense>
   );
 }
