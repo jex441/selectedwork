c98bf717a38121f5382b1d64fe70e9007458682f
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-05 14:05:44 -0400

    thumbnails size

--- app/[username]/[collection]/piece.tsx
+++ app/[username]/[collection]/piece.tsx
@@ -13,14 +13,14 @@ export default function piece({ data }: { data: IWork }) {
       <section
         key={data.id}
         onClick={() => setModal(true)}
-        className="border-1 relative mx-1 mb-8 grid h-auto w-full cursor-pointer justify-items-stretch gap-3 lg:mx-3 lg:h-[300px] lg:w-auto"
+        className="border-1 relative mx-1 mb-8 grid h-auto w-full cursor-pointer justify-items-stretch gap-3 lg:mx-3 lg:h-[340px] lg:w-auto"
       >
         <Image
           width={0}
           height={0}
           alt="work"
           sizes="100vw"
-          className="max-h-[620px] w-full self-center object-contain lg:max-h-[270px]"
+          className="max-h-[620px] w-full self-center object-contain lg:h-[310px]"
           src={data.media[0].url ?? ''}
         />
         <div className="flex w-full self-start self-end text-xs tracking-wide lg:mt-0 lg:w-auto">
