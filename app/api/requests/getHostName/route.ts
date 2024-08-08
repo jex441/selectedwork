c0a18499d7db4143d5b6bd3958ae5a37f839bc8c
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-08 19:59:13 -0400

    logging headers

--- app/api/requests/getHostName/route.ts
+++ app/api/requests/getHostName/route.ts
@@ -6,6 +6,6 @@ import { getCollectionDataForSite } from '@/app/lib/data';
 export async function GET(req: Request, res: NextApiResponse) {
   const headers = req.headers;
   const host: string = headers.get('host') || '';
-
+  console.log('headers::', headers);
   return NextResponse.json({ status: 200, data: { host } });
 }
