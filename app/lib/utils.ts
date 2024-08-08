c07d33b8ed0c529ebf5516185f57e45336b45581
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-08 19:13:39 -0400

    removed unused code

--- app/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-export const identifierHandler = (username: string, hostname: string) => {
-  if (hostname === 'localhost') return username;
-  if (hostname === 'selected-work.com') return username;
-  if (hostname === 'dash.xi-seven.vercel.app') return username;
-  else return hostname;
-};
