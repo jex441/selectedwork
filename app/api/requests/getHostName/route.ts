430ec13402a197e53a44a748275db825dea5567b
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-08 20:46:10 -0400

    res.json

--- app/api/requests/getHostName/route.ts
+++ app/api/requests/getHostName/route.ts
@@ -7,9 +7,10 @@ export async function GET(req: Request, res: NextApiResponse) {
   const headers = req.headers;
   const host: string = headers.get('host') || '';
   console.log('headers::', headers);
+
   if (host) {
-    return NextResponse.json({ status: 200, data: { host: host } });
+    return res.json({ status: 200, data: { host: host } });
   } else {
-    return NextResponse.json({ status: 400, data: { host: null } });
+    return res.json({ status: 400, data: { host: null } });
   }
 }
