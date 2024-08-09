7084719badea8474af722964c342a9c930639bac
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-08 20:40:13 -0400

    error handling on hostname route

--- app/api/requests/getHostName/route.ts
+++ app/api/requests/getHostName/route.ts
@@ -7,5 +7,9 @@ export async function GET(req: Request, res: NextApiResponse) {
   const headers = req.headers;
   const host: string = headers.get('host') || '';
   console.log('headers::', headers);
-  return NextResponse.json({ status: 200, data: { host: host, other: req } });
+  if (host) {
+    return NextResponse.json({ status: 200, data: { host: host } });
+  } else {
+    return NextResponse.json({ status: 400, data: { host: null } });
+  }
 }
