3e385ebc4835186f676d25ec398d3b3dbf9ac4b4
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-08 20:23:07 -0400

    returning whole req

--- app/api/requests/getHostName/route.ts
+++ app/api/requests/getHostName/route.ts
@@ -7,5 +7,5 @@ export async function GET(req: Request, res: NextApiResponse) {
   const headers = req.headers;
   const host: string = headers.get('host') || '';
   console.log('headers::', headers);
-  return NextResponse.json({ status: 200, data: { host } });
+  return NextResponse.json({ status: 200, data: { host: host, other: req } });
 }
