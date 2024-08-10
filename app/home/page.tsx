57b6255f9b4c0237d86e075945bffb484380595b
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-10 18:57:51 -0400

    routing properly

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -41,13 +41,13 @@ export default function page() {
             >
               Get Started
             </Link>
-            <Link
-              href="/dashboard"
+            <a
+              href={`http://app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`}
               className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-background px-8 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-              prefetch={false}
+              // prefetch={false}
             >
               Dashboard
-            </Link>
+            </a>
           </div>
         </div>
       </main>
