92f50032b08d233ac15b71e1f020c9bf0254c35c
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-14 10:05:45 -0400

    init stripe

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,21 @@
+// This is your test secret API key.
+const stripe = require('stripe')('');
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+export async function POST(req: Request, res: Response) {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: '{{PRICE_ID}}',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+  });
+
+  res.redirect(303, session.url);
+}
