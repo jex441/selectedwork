34e2e147ed610071fa8de1b34a0141a64867945e
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-16 16:25:10 -0400

    patch/about

--- app/[domain]/about/page.tsx
+++ app/[domain]/about/page.tsx
@@ -32,8 +32,8 @@ export default async function About({
 
   return (
     <main className="flex w-full flex-col items-start justify-center lg:flex-row lg:gap-10 lg:pt-10">
-      <section className="fade-in-up-simple relative flex h-[300px] w-full flex-col object-contain p-1 lg:h-[490px] lg:w-1/3">
-        <div className="relative h-auto w-full lg:max-h-[480px]">
+      <section className="fade-in-up-simple relative mb-2 flex h-[300px] w-full flex-col object-contain p-1 lg:mb-0 lg:h-[490px] lg:w-1/3">
+        <div className="relative max-h-[280px] w-full lg:max-h-[480px]">
           {imgSrc && (
             <Image
               height={0}
@@ -45,29 +45,29 @@ export default async function About({
             />
           )}
         </div>
-        <div className="text-darkGray mt-2 text-sm italic">{imgCaption}</div>
+        <div className="mt-2 text-sm italic text-darkGray">{imgCaption}</div>
       </section>
       <section className="fade-in-right-simple m-1  w-full lg:w-1/2 lg:pr-20">
-        <h1 className="text-darkGray text-xl leading-9">{heading}</h1>
-        <h3 className="text-mediumGray text-sm leading-9">{subheading}</h3>
+        <h1 className="text-xl leading-9 text-darkGray">{heading}</h1>
+        <h3 className="text-sm leading-9 text-mediumGray">{subheading}</h3>
         {text &&
           text
             .split('\r\n')
             .map((paragraph: string) => (
-              <p className="text-mediumGray my-2 text-xs leading-7">
+              <p className="my-2 text-xs leading-7 text-mediumGray">
                 {paragraph}
               </p>
             ))}
 
         {linkSrc1 && (
-          <p className="text-mediumGray hover:text-darkGray my-2 text-sm leading-7 underline">
+          <p className="my-2 text-sm leading-7 text-mediumGray underline hover:text-darkGray">
             <a href={linkSrc1} target="_blank">
               {linkText1}
             </a>
           </p>
         )}
         {linkSrc2 && (
-          <p className="text-mediumGray hover:text-darkGray my-2 text-sm leading-7 underline">
+          <p className="my-2 text-sm leading-7 text-mediumGray underline hover:text-darkGray">
             <a href={linkSrc2} target="_blank">
               {linkText2}
             </a>
