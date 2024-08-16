bd48c7c427d5fe7f9a0a85a92a3debac58668223
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-16 15:52:43 -0400

    not-found

---
+++ app/[domain]/nav/not-found.tsx
@@ -0,0 +1,9 @@
+export default async function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="font-cal text-4xl">404</h1>
+
+      <p className="text-lg text-stone-500">Page Not Found</p>
+    </div>
+  );
+}
--- app/[domain]/nav/page.tsx
+++ app/[domain]/nav/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { getUserByUsername } from '../../lib/requests';
 import Nav from './Nav';
+import { notFound } from 'next/navigation';
 
 export default async function page({ params }: { params: { domain: string } }) {
   const domain = decodeURIComponent(params.domain);
   const res = await getUserByUsername(domain);
 
+  if (res !== null && res.hibernate) {
+    notFound();
+  }
   if (res !== null) {
     return (
       <Nav collections={res.collections ?? []} displayName={res.displayName} />
