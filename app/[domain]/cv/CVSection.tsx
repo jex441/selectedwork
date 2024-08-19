05c4ce8d7201b70d082257d0211fa8f1d65e15af
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-08-19 16:20:46 -0400

    cv bugs fixed, styling

--- app/[domain]/cv/CVSection.tsx
+++ app/[domain]/cv/CVSection.tsx
@@ -12,7 +12,7 @@ export default function CVSection({
 }) {
   return (
     <section className="fade-in-up-simple mb-6 flex w-full flex-col lg:w-2/3">
-      <h1 className="mb-1 uppercase text-mediumGray lg:text-lg">{heading}</h1>
+      <h1 className="mb-1 uppercase text-lightGray lg:text-lg">{heading}</h1>
       {data &&
         data[categoryId] &&
         data[categoryId].map(
