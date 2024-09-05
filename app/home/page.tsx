8b6b5abbf77b7fdac631dc67b8b5d4fce7a5967d
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-09-05 07:28:13 -0400

    get started

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -34,13 +34,13 @@ export default function page() {
             A platform for showcasing your best work.
           </p>
           <div className="flex flex-col gap-4 sm:flex-row sm:justify-center">
-            <Link
-              href="/sign-up"
+            <a
+              href={`http${process.env.NODE_ENV === 'production' ? 's' : ''}://app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/sign-up`}
               className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-              prefetch={false}
+              // prefetch={false}
             >
               Get Started
-            </Link>
+            </a>
             <a
               href={`http://app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`}
               className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-background px-8 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
