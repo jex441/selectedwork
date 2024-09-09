50654c9f4832b21a0ea0cb6b481e7f757248a443
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-09-09 10:44:06 -0400

    footer on sites

--- app/[domain]/layout.tsx
+++ app/[domain]/layout.tsx
@@ -40,7 +40,20 @@ export default async function SiteLayout({
   return (
     <div className="flex min-h-screen w-full flex-col">
       <NavPage params={params} />
-      <main className="mt-[70px] lg:mt-0">{children}</main>
+      <main className="mt-[70px] min-h-screen lg:mt-0">{children}</main>
+      <div className="mt-10 flex h-20 w-[90%] flex-row justify-between self-center border-t border-gray-200 p-4 text-xs text-lightGray">
+        <div>{res.displayName} 2024</div>
+        {res.plan === 'free' && (
+          <div>
+            <a
+              href={`http://${process.env.NODE_ENV === 'production' ? 's' : ''}${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`}
+              className="transition-colors hover:text-darkGray hover:underline"
+            >
+              selected-work.com
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
