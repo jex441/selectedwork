fd55bb81495ff213198b4db9775dff340bf732e4
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-09-18 15:53:33 -0400

    commented out

--- app/[domain]/layout.tsx
+++ app/[domain]/layout.tsx
@@ -17,14 +17,6 @@ export default async function SiteLayout({
     return <Home />;
   }
   const res = await getUserByUsername(domain);
-  // Optional: Redirect to custom domain if it exists
-  //   if (
-  //     domain.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) &&
-  //     data.customDomain &&
-  //     process.env.REDIRECT_TO_CUSTOM_DOMAIN_IF_EXISTS === 'true'
-  //   ) {
-  //     return redirect(`https://${data.customDomain}`);
-  //   }
 
   if (!res) {
     return <div>user not found</div>;
