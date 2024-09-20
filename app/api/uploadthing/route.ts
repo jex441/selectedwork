b97d75c4ccc47a3abd2337a4a10c14481c0281b4
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-09-20 06:48:47 -0400

    uploadthing v 7

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -2,13 +2,9 @@ import { createRouteHandler } from 'uploadthing/next';
 
 import { ourFileRouter } from './core';
 // Export routes for Next App Router
-const handler = createRouteHandler({
+// Export routes for Next App Router
+export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-
   // Apply an (optional) custom config:
   // config: { ... },
-  config: {
-    token: process.env.UPLOADTHING_TOKEN,
-  },
 });
-export { handler as GET, handler as POST };
