2321ee1fb16a792e5c2c4a2ed37d9dd8736d071e
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-10-04 21:05:18 -0400

    dimensions

--- app/[domain]/[collection]/_templates/2/piece.tsx
+++ app/[domain]/[collection]/_templates/2/piece.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { IWork } from '@/app/interfaces/IWork';
 import Modal from './Modal';
+import { imageDimensionsFromStream } from 'image-dimensions';
 
 export default function Piece({
   data,
@@ -23,6 +24,16 @@ export default function Piece({
   const [src, setSrc] = useState<string>(
     data.media.find((m) => m.main === 'true')?.url || '',
   );
+  const [ratio, setRatio] = useState(0);
+
+  const getBody = async () => {
+    const { body } = await fetch(src);
+    const dimensions = await imageDimensionsFromStream(body);
+    setRatio(dimensions.width / dimensions.height);
+  };
+
+  getBody();
+
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
@@ -62,7 +73,7 @@ export default function Piece({
         ref={domRef}
         key={data.id}
         onClick={() => setModal(true)}
-        className={`${data.id !== null && data.id % 4 === 0 && 'animDelay'} fade-in-from-bottom relative flex cursor-pointer flex-col justify-between gap-3 lg:mx-0 lg:h-[500px] lg:flex-row lg:items-end lg:gap-0`}
+        className={`${data.id !== null && data.id % 4 === 0 && 'animDelay'} /fade-in-from-bottom relative flex cursor-pointer flex-col justify-between gap-3 bg-red-100 lg:mx-0 lg:max-h-[500px] lg:flex-row lg:items-end lg:gap-0`}
       >
         <Image
           width={0}
@@ -71,6 +82,11 @@ export default function Piece({
           sizes="100vw"
           className="lg:max-w-[700px]/ max-h-[620px] w-full object-contain lg:max-h-[500px] lg:max-w-[600px]"
           src={src}
+          onLoad={(e) => {
+            const { naturalWidth, naturalHeight } = e.target;
+            console.log('Image width:', naturalWidth);
+            console.log('Image height:', naturalHeight);
+          }}
         />
         <div className="flex w-full flex-row gap-1 self-end text-xs tracking-wide lg:mt-0 lg:w-auto lg:flex-col">
           <span className="text-[12px] text-mediumGray">{data.title}</span>
