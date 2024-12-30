8b81a4140b63088edcc1b2c3602a1fd7b82de18e
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-12-30 11:14:46 -0500

    hw500

--- app/[domain]/[collection]/_templates/2/piece.tsx
+++ app/[domain]/[collection]/_templates/2/piece.tsx
@@ -82,8 +82,8 @@ export default function Piece({
       >
         <Image
           onClick={() => clickHandler()}
-          width={0}
-          height={0}
+          width={500}
+          height={500}
           alt="work"
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 500px"
           className="max-h-[620px] w-full cursor-pointer object-contain lg:max-h-[500px] lg:max-w-[600px] lg:max-w-[700px]"
