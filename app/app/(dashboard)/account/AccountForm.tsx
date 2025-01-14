207f3181716c887d6d7dbac55e326bebca4d32ab
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-01-14 15:12:13 -0500

    removed occupation field

--- app/app/(dashboard)/account/AccountForm.tsx
+++ app/app/(dashboard)/account/AccountForm.tsx
@@ -22,47 +22,36 @@ export default function AccountForm({ data }: { data: IUser }) {
   const [state, formAction] = useFormState(updateUserWithId, initialState);
 
   return (
-      <form action={formAction}>
-            <Card>
-              <CardHeader>
-                <CardTitle>Account Settings</CardTitle>
-                <CardDescription>
-                  Update your personal information.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-6">
-                <div className="grid gap-4 md:grid-cols-2">
-                  <div className="grid gap-2">
-                    <Label htmlFor="name">Display Name</Label>
-                    <Input
-                      name="displayName"
-                      id="name"
-                      defaultValue={data.displayName || undefined}
-                    />
-                  </div>
-                  <div className="grid gap-2">
-                    <Label htmlFor="username">Username</Label>
-                    <Input
-                      name="username"
-                      id="username"
-                      defaultValue={data.username || undefined}
-                    />
-                  </div>
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="occupation">Occupation</Label>
-                  <Input
-                    id="occupation"
-                    name="occupation"
-                    defaultValue={data.occupation || undefined}
-                    placeholder="eg. Sculptor"
-                  />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button type="submit">Save Changes</Button>
-              </CardFooter>
-            </Card>
-      </form>
+    <form action={formAction}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Account Settings</CardTitle>
+          <CardDescription>Update your personal information.</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-6">
+          <div className="grid gap-4 md:grid-cols-2">
+            <div className="grid gap-2">
+              <Label htmlFor="name">Display Name</Label>
+              <Input
+                name="displayName"
+                id="name"
+                defaultValue={data.displayName || undefined}
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="username">Username</Label>
+              <Input
+                name="username"
+                id="username"
+                defaultValue={data.username || undefined}
+              />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button type="submit">Save Changes</Button>
+        </CardFooter>
+      </Card>
+    </form>
   );
 }
