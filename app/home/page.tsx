afb9c7c281058ff57d49260f8e5240d4fa685c58
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-02-06 11:54:03 -0500

    pricing

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -232,23 +232,25 @@ export default function page() {
         </div>
       </section>
 
-      {/* Only the features you need  */}
+      {/* Made for artwork  */}
       <section className="flex h-auto flex-col items-start justify-start bg-[#C7C7B0] p-6 md:min-h-[100vh] md:p-20">
         <div className="w-5/6">
-          <h1 className="text-6xl font-semibold uppercase leading-tight md:text-[116px] md:leading-[116px]">
-            Only the features you need.
-            <br /> <span className="text-white">Nothing you don’t.</span>
+          <h1 className="text-5xl font-semibold uppercase leading-tight md:text-[116px] md:leading-[116px]">
+            Made for displaying artwork.
+            <br /> <span className="text-white">Nothing else.</span>
           </h1>
-          <div className="mt-6 w-full text-lg leading-relaxed md:mt-12 md:text-xl">
+          <div className="mt-6 w-full text-lg md:mt-12 md:text-xl">
             <p className="leading-relaxed">
-              Stop paying for a ton of features you don't even use. SelectedWork
-              offers portfolio website templates that work, right out of the
-              box.
+              Unlike Squarespace and Wix which cater to every industry under the
+              sun, SelectedWork was purpose built for one thing, and one thing
+              only: displaying real, physical artwork, on the web - the right
+              way.
             </p>
           </div>
         </div>
       </section>
 
+      {/* Thumbnails */}
       <section className="flex h-auto flex-col items-center justify-center bg-[#D1CEC5] p-5 text-[#283739] md:h-[100vh] md:flex-row md:p-10">
         <div className="flex w-full flex-col justify-center space-y-4 p-5 md:w-1/3">
           <h2 className="text-2xl md:text-4xl">Get the whole picture</h2>
@@ -272,6 +274,7 @@ export default function page() {
         </div>
       </section>
 
+      {/* Dashboard */}
       <section className="flex h-auto flex-col items-center justify-center bg-[#EBEBEB] p-5 text-[#393E46] md:h-[100vh] md:flex-row md:p-10">
         <div className="flex w-full flex-col justify-center space-y-4 p-5 md:w-1/3">
           <h2 className="text-2xl md:text-4xl">
@@ -295,7 +298,7 @@ export default function page() {
       </section>
 
       {/* Whitespace  */}
-      <section className="flex h-auto flex-col items-start justify-start bg-[#C7C7B0] p-6 md:min-h-[100vh] md:p-20">
+      <section className="flex h-auto flex-col items-start justify-start bg-[#C7CFBE] p-6 md:min-h-[100vh] md:p-20">
         <div className="w-4/6">
           <h1 className="text-6xl font-semibold uppercase leading-tight md:text-[116px] md:leading-[116px]">
             Just the right amount of
@@ -303,27 +306,31 @@ export default function page() {
           </h1>
           <div className="mt-6 w-full text-lg leading-relaxed md:mt-12 md:text-xl">
             <p className="leading-relaxed">
-              Ditch the annoying, jumbled tile layouts and show your work in a
-              neat grid and give your work space to breathe.
+              Ditch the annoying, jumbled, tile layouts, show your work in a
+              clean, neat, grid, and give your work space to breathe.
             </p>
           </div>
         </div>
-        <div className="flex w-full flex-row items-center justify-center">
-          <div>
-            Before
+        <div className="my-10 flex w-full flex-row items-center justify-around">
+          <div className="flex flex-col items-center justify-center gap-5">
+            <span className="text-sm font-semibold uppercase tracking-wider text-gray-600">
+              Before
+            </span>
             <video
-              src="/whitespace.mov"
-              className="h-[300px] rounded-md shadow-md md:h-[450px]"
+              src="/beforevideo.mov"
+              className="h-[300px] rounded-md shadow-md md:h-[320px]"
               autoPlay
               loop
               muted
             ></video>
           </div>
-          <div>
-            After
+          <div className="flex flex-col items-center justify-center gap-5">
+            <span className="tracking-widetext-sm r font-semibold uppercase text-gray-600">
+              After
+            </span>
             <video
-              src="/whitespace.mov"
-              className="h-[300px] rounded-md shadow-md md:h-[450px]"
+              src="/aftervideo.mov"
+              className="h-[300px] rounded-md shadow-md md:h-[320px]"
               autoPlay
               loop
               muted
@@ -332,21 +339,71 @@ export default function page() {
         </div>
       </section>
 
-      {/* Made for artwork  */}
+      {/* Only the features you need  */}
       <section className="flex h-auto flex-col items-start justify-start bg-[#CDD5E0] p-6 md:min-h-[100vh] md:p-20">
         <div className="w-5/6">
-          <h1 className="text-5xl font-semibold uppercase leading-tight md:text-[116px] md:leading-[116px]">
-            Made for displaying artwork.
-            <br /> <span className="text-white">Nothing else.</span>
+          <h1 className="text-6xl font-semibold uppercase leading-tight md:text-[116px] md:leading-[116px]">
+            Only the features you need.
+            <br /> <span className="text-white">Nothing you don’t.</span>
           </h1>
-          <div className="mt-6 w-full text-lg md:mt-12 md:text-xl">
+          <div className="mt-6 w-full text-lg leading-relaxed md:mt-12 md:text-xl">
             <p className="leading-relaxed">
-              Unlike Squarespace and Wix which cater to every industry, Selected
-              Work was built for one thing, and one thing only: displaying real,
-              physical artwork on the web, the right way.
+              Stop overpaying for a ton of features you don't even use.
+              SelectedWork offers portfolio website templates that work, right
+              out of the box.
             </p>
           </div>
         </div>
+
+        {/* Pricing Section */}
+        <section className="flex w-full flex-col items-center justify-center md:p-5">
+          <div className="mt-10 flex flex-col gap-20 md:flex-row">
+            {/* Free Tier */}
+            <div className="flex h-[470px] w-[320px] max-w-md flex-col rounded-lg border-2 border-gray-600 p-10 text-center md:p-10">
+              <h3 className="mb-2 bg-gray-600 p-1 text-xl font-semibold uppercase tracking-wide text-[#CDD5E0] text-gray-600">
+                Hobby
+              </h3>
+              <p className="my-2 text-gray-500">Start Building</p>
+              <h3 className="text-2xl font-semibold leading-loose text-gray-600">
+                Free
+              </h3>
+              <ul className="mt-4 h-[150px] space-y-2 text-left text-gray-700">
+                <li>All the tools you need to show your work</li>
+              </ul>
+              <div className="mt-6 flex flex-col items-center justify-center gap-2">
+                <div className="relative w-full max-w-[180px] cursor-pointer overflow-hidden border-2 border-gray-600 bg-transparent px-6 py-2 text-lg font-medium text-gray-600 transition-all before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:bg-gray-600 before:text-[#CDD5E0] before:transition-all before:duration-100 hover:text-[#CDD5E0] hover:before:h-full">
+                  <span className="relative z-10 text-sm font-semibold uppercase">
+                    Try it out
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            {/* Pro Tier */}
+            <div className="flex h-[470px] w-[320px] max-w-md flex-col  rounded-lg border-2 border-gray-600 p-10 text-center md:p-10">
+              <h3 className="mb-2 bg-gray-600 p-1 text-xl font-semibold uppercase tracking-wide text-[#CDD5E0] text-gray-600">
+                Pro
+              </h3>
+              <p className="my-2 text-gray-500">Make it your own</p>
+              <h3 className="text-2xl font-semibold leading-loose text-gray-600">
+                $10/month
+              </h3>
+              <ul className="mt-4 h-[150px] space-y-2 text-left text-gray-700">
+                <li>Custom domains</li>
+                <li>No branding</li>
+                <li>Pause anytime</li>
+                <li>Cancel anytime</li>
+              </ul>
+              <div className="mt-6 flex flex-col items-center justify-center gap-2">
+                <div className="relative w-full max-w-[180px] cursor-pointer overflow-hidden border-2 border-gray-600 bg-transparent px-6 py-2 text-lg font-medium text-gray-600 transition-all before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:bg-gray-600 before:text-[#CDD5E0] before:transition-all before:duration-100 hover:text-[#CDD5E0] hover:before:h-full">
+                  <span className="relative z-10 text-sm font-semibold uppercase">
+                    Go Pro
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </section>
 
       <section className="flex h-auto flex-col items-center justify-between bg-[#CDCACE] py-8 text-[#393E46] md:h-[100vh] md:flex-row md:py-10">
