c77f10399cbb751cdaff8bf4e8b61b8a96414a0e
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-03-18 15:08:49 -0400

    about consolidated

--- app/app/(dashboard)/about/page.tsx
+++ app/app/(dashboard)/about/page.tsx
@@ -4,11 +4,9 @@ import AboutForm from './aboutform';
 import { getPageData } from '../../../lib/data';
 
 export default async function Component() {
-  const data: IAboutPage | undefined = await getPageData('About');
-
   return (
     <div className="container mx-auto py-4 md:px-4 md:px-6 lg:px-8">
-      {data && <AboutForm data={data} />}
+      <AboutForm />
     </div>
   );
 }
