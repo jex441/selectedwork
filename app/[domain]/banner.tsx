c30e72518d892422a8df46dcd34319b227f4a183
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-04-09 13:09:10 -0400

    banner mobile

--- app/[domain]/banner.tsx
+++ app/[domain]/banner.tsx
@@ -19,17 +19,15 @@ export default function Banner() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1 }}
           exit={{ y: 200 }}
-          className="fixed bottom-0 my-5 flex w-full flex-col items-start justify-start self-center rounded-full bg-lime-300 p-4 px-10 shadow-lg lg:m-10 lg:h-[120px] lg:w-[800px] lg:text-xl"
+          className="fixed bottom-0 my-5 flex w-full flex-col items-start justify-start self-center rounded-full bg-lime-300 px-6 py-1 text-sm shadow-lg lg:m-10 lg:h-[120px] lg:w-[800px] lg:px-10 lg:py-5 lg:text-xl"
         >
           <button
             onClick={handleCloseBanner}
-            className="absolute right-10 top-5 text-gray-600 transition-all hover:text-black"
+            className="absolute right-5 top-2 text-gray-600 transition-all hover:text-black lg:right-10 lg:top-5"
           >
-            <X />
+            <X height={20} width={20} />
           </button>
-          <h1 className="leading-loose text-gray-600 lg:text-2xl">
-            Want a website like this?
-          </h1>
+          <h1 className="lg:mb-2 lg:text-2xl">Want a website like this?</h1>
           <p>
             Get started on
             <a href="https://selectedwork.net" className="mx-1 underline">
