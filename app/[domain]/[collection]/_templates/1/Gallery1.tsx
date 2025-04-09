f026c20a26da9e894ba66fecdc17acbef842abbf
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-04-09 11:39:56 -0400

    padding on top section

--- app/[domain]/[collection]/_templates/1/Gallery1.tsx
+++ app/[domain]/[collection]/_templates/1/Gallery1.tsx
@@ -25,9 +25,9 @@ export default function page({ data, user }: { data: ICollection; user: any }) {
 
   return (
     <main className="flex w-full flex-wrap justify-center">
-      <section className="fade-in-up-simple flex flex-col justify-center lg:w-4/5 lg:flex-row lg:gap-10">
-        {imgSrc && (
-          <>
+      {imgSrc && (
+        <>
+          <section className="fade-in-up-simple mb-10 flex flex-col justify-center lg:w-4/5 lg:flex-row lg:gap-10">
             <div className="mx-1 flex flex-col lg:m-5 lg:mx-0 lg:h-[400px] lg:w-1/2 lg:w-[500px]">
               <div className="relative w-full">
                 <Image
@@ -46,50 +46,53 @@ export default function page({ data, user }: { data: ICollection; user: any }) {
                 </span>
               </div>
             </div>
-          </>
-        )}
 
-        <div className="fade-in-right-simple mx-1 my-5 flex-1 lg:m-5 lg:w-1/2">
-          <h1 className="text-xl leading-9 text-mediumGray">
-            {imgSrc || subheading || linkSrc1 || linkSrc2 || description
-              ? title
-              : null}
-          </h1>
-          <h3 className="text-sm leading-9 text-mediumGray">{subheading}</h3>
-          <div className="text-xs leading-7 text-mediumGray">
-            {description &&
-              description
-                .split('\r\n')
-                .map((paragraph: string) => (
-                  <p className="my-2 text-xs leading-7 text-mediumGray">
-                    {paragraph}
-                  </p>
-                ))}
-          </div>
-          <p className="my-4 text-sm text-mediumGray">
-            {linkSrc1 && (
-              <a
-                href={linkSrc1}
-                className="underline hover:text-black"
-                target="_blank"
-              >
-                {linkText1}
-              </a>
-            )}
-          </p>
-          <p className="text-sm leading-7 text-gray-600">
-            {linkSrc2 && (
-              <a
-                href={linkSrc2}
-                className="underline hover:text-black"
-                target="_blank"
-              >
-                {linkText2}
-              </a>
-            )}
-          </p>
-        </div>
-      </section>
+            <div className="fade-in-right-simple mx-1 my-5 flex-1 lg:m-5 lg:w-1/2">
+              <h1 className="text-xl leading-9 text-mediumGray">
+                {imgSrc || subheading || linkSrc1 || linkSrc2 || description
+                  ? title
+                  : null}
+              </h1>
+              <h3 className="text-sm leading-9 text-mediumGray">
+                {subheading}
+              </h3>
+              <div className="text-xs leading-7 text-mediumGray">
+                {description &&
+                  description
+                    .split('\r\n')
+                    .map((paragraph: string) => (
+                      <p className="my-2 text-xs leading-7 text-mediumGray">
+                        {paragraph}
+                      </p>
+                    ))}
+              </div>
+              <p className="my-4 text-sm text-mediumGray">
+                {linkSrc1 && (
+                  <a
+                    href={linkSrc1}
+                    className="underline hover:text-black"
+                    target="_blank"
+                  >
+                    {linkText1}
+                  </a>
+                )}
+              </p>
+              <p className="text-sm leading-7 text-gray-600">
+                {linkSrc2 && (
+                  <a
+                    href={linkSrc2}
+                    className="underline hover:text-black"
+                    target="_blank"
+                  >
+                    {linkText2}
+                  </a>
+                )}
+              </p>
+            </div>
+          </section>
+        </>
+      )}
+
       <section className="mx-6 mb-10 grid w-full grid-cols-1 justify-items-center gap-x-2 gap-y-16 md:grid-cols-2 lg:grid-cols-4 lg:px-20">
         {works &&
           data.works.map((work: IWork, index: number) => (
