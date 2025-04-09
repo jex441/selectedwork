ff8c3484e541d798cfc9e862152fcb263e33ae2e
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-04-09 13:00:29 -0400

    banner

---
+++ app/[domain]/banner.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'motion/react';
+
+import { X } from 'lucide-react';
+export default function Banner() {
+  const [isBannerVisible, setBannerVisible] = useState(true);
+
+  const handleCloseBanner = () => {
+    setBannerVisible(false);
+  };
+
+  return (
+    <AnimatePresence>
+      {isBannerVisible && (
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1 }}
+          exit={{ y: 200 }}
+          className="fixed bottom-0 m-10 flex h-10 h-[120px] w-[800px] w-full flex-col items-start justify-start self-center rounded-full bg-lime-300 p-4 px-10 text-xl shadow-lg"
+        >
+          <button
+            onClick={handleCloseBanner}
+            className="absolute right-10 top-5 text-gray-600 transition-all hover:text-black"
+          >
+            <X />
+          </button>
+          <h1 className="text-2xl leading-loose text-gray-600">
+            Want a website like this?
+          </h1>
+          <p>
+            Get started on
+            <a href="https://selectedwork.net" className="mx-1 underline">
+              SelectedWork
+            </a>
+            and show your work with confidence.
+          </p>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
